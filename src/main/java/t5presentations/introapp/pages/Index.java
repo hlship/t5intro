ec5bdf8733953e81f35e3d7ec3d5a3a38e8f46ce
Howard M. Lewis Ship <hlship@gmail.com>
2009-11-08 16:42:57 -0800

    Add saving of the new board game, and confirmation message.

--- src/main/java/t5presentations/introapp/pages/Index.java
+++ src/main/java/t5presentations/introapp/pages/Index.java
@@ -1,14 +1,32 @@
 package t5presentations.introapp.pages;
 
-import java.util.Date;
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import t5presentations.introapp.entities.BoardGame;
 
 /**
  * Start page of application introapp.
  */
 public class Index
 {
-	public String getCurrentTime() 
-	{ 
-		return "A great time to get started with Tapestry!";
-	}
+  @Property
+  private BoardGame game;
+  
+  @Property
+  @Persist(PersistenceConstants.FLASH)
+  private String message;
+  
+  @Inject
+  private Session session;
+  
+  void onSuccess()
+  {
+    session.save(game);
+    
+    message = "Added board game: " + game.getTitle();
+  }
 }
